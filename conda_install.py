1b680cefc54f59eb083525d293fe356ed2b706ff
AJ Medford <ajmedfor@gmail.com>
2017-04-07 16:15:06 -0400

    fixed conda installer

--- conda_install.py
+++ conda_install.py
@@ -1,15 +1,13 @@
 import sys
 import os
 
+if len(sys.argv) < 2:
+    print('Pass the name of the environment as the argument')
 env = sys.argv[1]
 
 prefix = '/nv/hp22/amedford6/medford-shared/builds/anaconda/4.2.0/none/anaconda2/envs/'+env
 assert os.path.exists(prefix)
 
-path = prefix+'/lib/python2.7/site-packages'
-if not os.path.exists(path):
-    os.system('mkdir -p '+path)
+print('Installing espresso interface in: '+prefix)
 
-print('Installing espresso interface in: '+path)
-
-os.system('python setup.py install --prefix='+path)
+os.system('python setup.py install --prefix='+prefix)
